4c94b901dd57f556e42c644928f9147a9845c54d
paiai <leesu@leesuui-MacBook-Pro.local>
2018-04-27 00:24:11 +0900

    d3 example

---
+++ ex/server.js
@@ -0,0 +1,37 @@
+var SERVER_PORT = 1337;
+var FILE_DEFAULT = '/line.html';
+
+var http = require('http'),
+    URL  = require('url'),
+    path = require('path'),
+    fs = require('fs');
+
+var server = http.createServer(checkRequest);
+server.listen(SERVER_PORT, function(){
+    console.log('server is running...');
+    console.log('http://localhost: ' + SERVER_PORT);
+});
+
+function checkRequest(req, res) {
+    var uri = URL.parse(req.url, true);
+    var pathname = uri.pathname;
+    if ( pathname == '/' ) pathname = FILE_DEFAULT;
+    console.log(pathname);
+
+    var filename = path.join(__dirname, pathname);
+    if ( !fs.existsSync(filename) ) {
+        res.writeHead(404, {'Content-Type':'text/html'});
+        res.end('404 file not found');
+        return;
+    }
+
+    var stat = fs.statSync(filename);
+    if( stat && stat.isDirectory() ) {
+        res.writeHead(403, {'Content-Type':'text/html'});
+        res.end('403');
+        return;
+    }
+
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(fs.readFileSync(filename, 'utf-8'));
+}
