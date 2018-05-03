59a066e23e7c5ef323affb558838cea97fbe5add
paiai <leesu@leesuui-MacBook-Pro.local>
2018-05-04 00:44:10 +0900

    D3 example

--- map_ex/server.js
+++ map_ex/server.js
@@ -6,10 +6,20 @@ var http = require('http'),
     path = require('path'),
     fs = require('fs');
 
+var mime = {
+    html: 'text/html',
+    txt: 'text/plain',
+    css: 'text/css',
+    gif: 'image/gif',
+    jpg: 'image/jpeg',
+    png: 'image/png',
+    js: 'application/javascript'
+};
+
 var server = http.createServer(checkRequest);
 server.listen(SERVER_PORT, function(){
     console.log('server is running...');
-    console.log('http://localhost: ' + SERVER_PORT);
+    console.log('http://localhost:' + SERVER_PORT);
 });
 
 function checkRequest(req, res) {
@@ -32,6 +42,57 @@ function checkRequest(req, res) {
         return;
     }
 
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end(fs.readFileSync(filename, 'utf-8'));
+    var filePath = '.' + req.url;
+    if (filePath == './')
+        filePath = './index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+        case '.svg':
+            contentType = 'image/svg+xml';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                });
+            }
+            else {
+                res.writeHead(500);
+                res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                res.end(); 
+            }
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+
+
+    // res.writeHead(200, {'Content-Type':'text/html'});
+    // res.end(fs.readFileSync(filename, 'utf-8'));
 }
